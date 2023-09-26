38866b620596aa7644e23a468c70d76f098cd93f
hvrishabh <hitesh.verma@rishabhsoft.com>
2023-09-26 19:04:36 +0530

    this is 1st fundamental commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,215 @@
+// let isIsland = "Andaman and Nicobar";
+
+// let language = true;
+
+// console.log(isIsland);
+// console.log(language);
+
+// console.log(typeof language);
+
+// language = "YEs";
+// console.log(typeof language);
+
+// console.log(typeof null);
+
+// ..........................// data types 2222222222222
+
+// let isIsland = "India";
+
+// let language;
+// let value = true;
+
+// console.log(typeof value);
+// console.log(typeof language);
+// console.log(typeof value);
+
+// .....................................................
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = "jonas";
+// const lastName = "Schmedtmann";
+
+// console.log(firstName + " " + lastName);
+
+// // console.log(`$firstName`);
+
+// let x = 10 + 5;
+// x += 10;
+// x /= 5;
+
+// x++;
+// x--;
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+
+// console.log(ageSarah >= 19);
+// // .....................................................
+
+// const now = 2037;
+// const firstName = "Jonas";
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// // ......................................Coding challenge 1...........
+
+/* Write your code below. Good luck! 🙂 */
+// const johnMass = 78;
+// const markMass = 92;
+
+// const johnHeight = 1.69;
+// const markHeight = 1.95;
+
+// let BMImark = markMass / markHeight ** 2;
+// console.log(BMImark);
+// let BMIjohn = johnMass / johnHeight ** 2;
+// console.log(BMIjohn);
+
+// const markHigherBMI = BMImark > BMIjohn;
+// console.log(markHigherBMI);
+
+//.................TEmplate Literals.................................
+
+// const now = 2037;
+// const firstName = "Jonas";
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// const jonasNew = `I'm ${firstName} , a  ${ageJonas} years old person and Sarah is ${ageSarah} years old. `;
+// console.log(jonasNew);
+
+// console.log(`Just a Regular string`);
+
+// console.log(
+//   "String with \n\
+// multiple \n line"
+// );
+
+// console.log(`sting
+// with
+// multiline`);
+
+// ///// ...................Driving Licence Age ..........
+
+// const age = 12;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//   console.log("sarash can start with driving licenece");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log("Sarah is too young. Wait another " + yearsLeft);
+// }
+// //.......................................
+
+// let century;
+// const birthYear = 1991;
+
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+
+// console.log(century);
+
+//..........................l-type Conversion
+
+// // type conversion , when we converts the data type explicitly or manually
+// const inputYear = "1991";
+// console.log(Number(inputYear) + 18);
+// console.log(inputYear + 18);
+// console.log(String(12));
+
+// // type coresion , when system automatically converts data type
+
+// console.log(inputYear - 10);
+
+// console.log("I am " + 23 + " years old");
+
+// ...................l -21 Truely and Falsey vales.........../////////
+
+// // 5 falsey valuse : 0, "", undefined, null , NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log("you should get a job");
+// }
+
+// let height = 12;
+// if (height) {
+//   console.log("height defined");
+// } else {
+//   console.log("height undefined");
+// }
+
+///////////////......................... l 22 ..........Equality operators
+
+// const age = 18;
+// if (age === 18) {
+//   console.log("you just became an adult");
+// }
+
+// let fav = prompt("whats your fav no");
+// console.log(fav);
+// console.log(typeof fav);
+
+/////////////////////..... l23 Boolearn logic Opertors .....................
+
+////////////.......L 24 logical Operators....................
+
+// const hasDriversLicence = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicence && hasGoodVision);
+// console.log(hasDriversLicence || hasGoodVision);
+// console.log(!hasDriversLicence);
+
+///////........l 26 Switch statement..........
+
+// const day = "tuesday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("Plan course structure.");
+//     break;
+//   case "tuesday":
+//   case "wednesday":
+//     console.log("sdfasdflldsa;f");
+//     break;
+
+//   default:
+//     console.log("FUNNNNNNNNNNNNNNNNa");
+// }
+
+////////............... L 27 Statements vs Expresssions
+
+// Expression , produces a values
+
+// statement, performs action but doesn't produces a value.
+
+/////////////////.................L 28 Ternary Operators ............
+
+// const age = 12;
+// age >= 18 ? console.log("I like to have drink") : console.log("drink water.");
+
+// let drink = age >= 18 ? "drink" : "water";
+// console.log(drink);
+
+////////.............. ES 6 ESA2015 ECMA Script............
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,113 @@
+// l32.... Strict Mode in JS
+
+"use strict";
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicence = true;
+
+// if (hasDriversLicence) console.log("i can drive");
+
+////////..........L 33 functions ...............
+
+// function is a piece of code which can be reused;
+
+// function logger() {
+//   console.log("My name is Jonas.");
+// }
+
+// // calling ,running, invoking the function .
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   let juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// let appleJuice = fruitProcessor();
+// console.log(appleJuice);
+
+// console.log(fruitProcessor(3, 4));
+
+/////////.......... L 34  Functional Decleration and Expression ...................
+
+// // we can call function declartion  before they are defined in the code.
+// function calcAge1(birthYear = 2000) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // ....... Another method to deckare the function
+// // Anonymous function
+
+// // this below code is an expression , which we have a value , and we have stored this into the calcAge2 variable .
+
+// // we can call function declartion  before they are defined in the code.
+// // but function expressions can not be called before they are defined
+
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+//////////////............. L 35 Arrow functions......
+
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// //Arrow function
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntillRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `The name is ${firstName} and the retirement age is ${retirement}`;
+// };
+
+// console.log(yearsUntillRetirement(1991, "Ramu"));
+
+////////////................ L 36 Calling a function Inside the other function ..........
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   console.log(applePieces, orangePieces);
+//   let juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+//   return juice;
+// }
+
+// const mixJuice = fruitProcessor(5, 6);
+// console.log(mixJuice);
+
+////////////////////////...................... L 37  REviewing functions ................
+
+const calcage = function (birthYear) {
+  return 2037 - birthYear;
+};
+const yearsUntillRetirement = function (birthYear, firstName) {
+  const age = calcage(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    return retirement;
+  } else {
+    return -1;
+  }
+};
+
+console.log(yearsUntillRetirement(1991, "Ram"));
+console.log(yearsUntillRetirement(1970, "Mike"));
